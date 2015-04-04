ba9005701314265a34d8c0ca99723aafc942cbc3
Daniel Berkompas <himself@danielberkompas.com>
2015-04-04 10:28:49 -0700

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule ExTwilio.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :travis_plt_generator,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     name: "Travis PLT Generator",
+     source_url: "https://github.com/danielberkompas/ex_twilio",
+     dialyzer: [
+       plt_file: "#{System.get_env("HOME")}/#{plt_filename}",
+       flags: ["--no_native"]
+     ],
+     deps: deps]
+  end
+
+  def application do
+    [applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp plt_filename do
+    "elixir-#{System.version}_#{otp_release}.plt"
+  end
+
+  defp otp_release do
+    case System.get_env("TRAVIS_OTP_RELEASE") do
+      nil     -> :erlang.system_info(:otp_release)
+      release -> release
+    end
+  end
+end
+
